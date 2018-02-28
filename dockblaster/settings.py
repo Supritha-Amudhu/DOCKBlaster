9e6230b8308413bc1c7d72578cf0b534d6f8d9e1
Chinzo Dandar <ChinzoD@users.noreply.github.com>
2018-02-28 13:49:07 -0800

    Update settings.py

--- dockblaster/settings.py
+++ dockblaster/settings.py
@@ -2,17 +2,19 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 POSTGRES = {
-    'user': 'supritha',
+    'user': 'blasteruser',
     'pw': '',
-    'db': 'dockblaster',
-    'host': 'localhost',
+    'db': 'blaster',
+    'host': 'mem.cluster.ucsf.bkslab.org',
     'port': '5432',
+    #'port': '6543',
 }
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'DOCK-Blaster'
     SESSION_TYPE = 'filesystem'
-    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/dockblaster"
+    #SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://blasteruser:@localhost:6543/blaster"
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://blasteruser:@mem.cluster.ucsf.bkslab.org:5432/blaster"
     APP_DIR = os.path.abspath(os.path.dirname(__file__))  # This directory
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
     SQLALCHEMY_TRACK_MODIFICATIONS = False

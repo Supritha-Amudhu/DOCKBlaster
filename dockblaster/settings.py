b53506ce04e7d2fde83d5727dd70ccf6448473be
Supritha Amudhu <supritha.amudhu@gmail.com>
2018-03-05 16:42:55 -0800

    Setting Parse File path as a variable in settings.py

--- dockblaster/settings.py
+++ dockblaster/settings.py
@@ -20,6 +20,7 @@ class Config(object):
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = '/nfs/ex7/blaster/jobs/1/'
+    PARSE_FOLDER = '/nfs/ex7/blaster/templates/'
     DEBUG = True
 
 
@@ -42,6 +43,7 @@ class DevConfig(Config):
     DB_PATH = os.path.join(Config.PROJECT_ROOT, DB_NAME)
     DEBUG_TB_ENABLED = True
     UPLOAD_FOLDER = expanduser("~") + '/DOCKBlaster/Files/'
+    PARSE_FOLDER = expanduser("~") + '/DOCKBlaster/Files/Types/'
     CACHE_TYPE = 'simple'  # Can be "memcached", "redis", etc.
     SQLALCHEMY_DATABASE_URI="postgresql://localhost/dockblaster"
 

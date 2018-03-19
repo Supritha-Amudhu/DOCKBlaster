49bd1f71636186cd3a41a04bd50807bd456aa626
Supritha Amudhu <supritha.amudhu@gmail.com>
2018-03-19 14:20:44 -0700

    Grid view for Job Types, running the command file for each job

--- dockblaster/helper.py
+++ dockblaster/helper.py
@@ -27,30 +27,8 @@ def generate_result_file(upload_folder, receptorFile_contents, ligandFile_conten
         fo.close()
 
 def parse_parameters_file(path_to_file):
-    # try:
-        data = json.load(open(path_to_file))
-        print data
-        job_parameters = {}
-        return data
-        # with open(path_to_file, 'r') as f:
-        #     job_parameters = {}
-        #     lines = f.readlines()
-        #     # print lines
-        #     lines = iter(lines)
-        #     for line in lines:
-        #         if(line == "jobtypename:\n"):
-        #             job_parameters["jobtypename"] = next(lines).replace("\n", "")
-        #         if(line == "fullname:\n"):
-        #             job_parameters["fullname"] = next(lines).replace("\n", "")
-        #     print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-        #     print job_parameters
-        #     return job_parameters
-    # finally:
-        # f.close()
-
-# def write_to_text_file(file, path_to_file):
-#
-
+    data = json.load(open(path_to_file))
+    return data
 
 def parse_file_name(path_to_file):
     job_types = [f for f in listdir(path_to_file) if isdir(join(path_to_file, f))]
